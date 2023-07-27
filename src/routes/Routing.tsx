3abb05e6003e2d6eca69faf85b19e12d73541634
vandanp2912 <vandan.gloryautotech@gmail.com>
2023-07-27 18:05:45 +0530

    routing components added

--- src/routes/Routing.tsx
+++ src/routes/Routing.tsx
@@ -3,6 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Auth from "../pages/Auth/Auth";
 import ProtectedRoute from "./ProtectedRoute";
+import Splash from "../pages/splash/Splash";
+import Login from "../pages/login/Login";
+import OTP from "../pages/otp/OTP";
+import Success from "../pages/success/Success";
+import Catalog from "../pages/catalog/Catalog";
+import Categories from "../pages/categories/Categories";
+import AddProduct from "../pages/addProduct/AddProduct";
+import ViewProduct from "../pages/viewProduct/ViewProduct";
+import Cart from "../pages/cart/Cart";
+import Address from "../pages/address/Address";
 
 const Routing = () => {
   return (
@@ -10,16 +20,16 @@ const Routing = () => {
       <Route path="auth" element={<Auth />} />
       <Route path="/" element={<ProtectedRoute />}>
         <Route index element={<Home />} />
-        <Route path="/splash" element={<Home />} />
-        <Route path="/login" element={<Home />} />
-        <Route path="/otp" element={<Home />} />
-        <Route path="/success" element={<Home />} />
-        <Route path="/catalog" element={<Home />} />
-        <Route path="/categories/:id" element={<Home />} />
-        <Route path="/addProduct/:id" element={<Home />} />
-        <Route path="/viewProduct/:id" element={<Home />} />
-        <Route path="/cart" element={<Home />} />
-        <Route path="/address" element={<Home />} />
+        <Route path="/splash" element={<Splash />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/otp" element={<OTP />} />
+        <Route path="/success" element={<Success />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/categories/:id" element={<Categories />} />
+        <Route path="/addProduct/:id" element={<AddProduct />} />
+        <Route path="/viewProduct/:id" element={<ViewProduct />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/address" element={<Address />} />
       </Route>
     </Routes>
   );
