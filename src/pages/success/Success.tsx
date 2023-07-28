4685d65de2b8450ce8d17bade76158c5ce028ddd
vandanp2912 <vandan.gloryautotech@gmail.com>
2023-07-28 15:51:39 +0530

    Success screen added

--- src/pages/success/Success.tsx
+++ src/pages/success/Success.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import Button from "../../components/Button";
 
 const Success = () => {
-    return (
-        <div>
-            Success Screen
-        </div>
-    );
+  return (
+    <div className="w-screen h-screen px-5 py-5">
+      <img src="/images/happy_sun.svg" alt="happy_sun_image" />
+      <div className="text-center my-6 text-2xl font-semibold">
+        <div className="text-primary">Congratulations!</div>
+        <div>Your account has been</div>
+        <div>successfully created</div>
+      </div>
+      <div className="px-5 text-sm font-normal flex text-justify">
+        Our intuitive platform ensures that you find exactly what you're looking
+        for, making your shopping experience delightful and efficient.
+      </div>
+      <div className="flex justify-center mt-5">
+        <Button title="Let's Go" />
+      </div>
+    </div>
+  );
 };
 
 export default Success;
